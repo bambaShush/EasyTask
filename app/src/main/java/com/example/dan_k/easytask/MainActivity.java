f9415dc23f7073290b6b58c776e01812453101ae
dan_k <dan_ko@outlook.com>
2018-07-26 13:11:26 +0300

    check commit and push

--- app/src/main/java/com/example/dan_k/easytask/MainActivity.java
+++ app/src/main/java/com/example/dan_k/easytask/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.dan_k.easytask;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SignUpFragment.OnLoginListener {
+    private Fragment signInFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        getSupportFragmentManager().beginTransaction().replace(R.id.showFrag,new SignUpFragment()).addToBackStack(null).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            /*case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+*/
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public void onLogin() {
+
     }
 }
